a6a543da7e59fc8e456d79230f617c37c3a8a8ad
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-18 19:22:49 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,8 +11,11 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.sort_by {|kids_name, age| age}
-  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  if age < 18
+    birthday_kids.each do |kids_name, age|
+    #birthday_kids.sort_by {|kids_name, age| age}
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end 
   end
 end 
 
