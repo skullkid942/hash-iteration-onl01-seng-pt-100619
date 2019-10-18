2b0c85fc9f7514e548876e7727ccaf0d808ad998
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-18 19:20:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,7 +11,7 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.sort 
+  birthday_kids.sort_by |kids_name, age
   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end 
